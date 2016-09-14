6a5e54952639ee1abb4fe8984ee5451c9f480efd
ssdraid0 <ssdraid0@163.com>
2016-09-14 21:55:17 +0800

    增加堆排序和归并排序

--- java/src/algorithm/SortQuestion.java
+++ java/src/algorithm/SortQuestion.java
@@ -8,18 +8,26 @@ package algorithm;
  * 归并排序：nlogn，nlogn，nlogn。<br>
  * 堆排序：nlogn，nlogn，nlogn。<br>
  * Timsort：n，nlogn，nlogn。<br>
+ * <br>
+ * 2.实现冒泡排序，快速排序，堆排序，归并排序。<br>
+ * 冒泡排序：{@link BubbleSortQuestion#sort(int[])}。<br>
+ * 快速排序：{@link QuickSortQuestion#sort(int[])}。<br>
+ * 堆排序：{@link HeapSortQuestion#sort(int[])}。<br>
+ * 归并排序：{@link MergeSortQuestion#sort(int[])}。<br>
  */
 public class SortQuestion
 {
     public static void main(String[] args)
     {
-        int[] a = { 2, 5, 3, 8, 7, 6 };
+        int[] a = { 2, 5, 3, 8, 7, 4 };
         // SelectSortQuestion.sort(a);
         // BubbleSortQuestion.sort(a);
-        QuickSortQuestion.sort(a);
+        // QuickSortQuestion.sort(a);
+        // HeapSortQuestion.sort(a);
+        MergeSortQuestion.sort(a);
         for (int i = 0; i < a.length; i++)
         {
-            System.out.print(i);
+            System.out.print(a[i]);
         }
     }
 }
@@ -63,7 +71,6 @@ class BubbleSortQuestion
 }
 
 /**
- * 1.<br>
  * {@link https://en.wikipedia.org/wiki/Quicksort}。
  */
 class QuickSortQuestion
@@ -96,5 +103,125 @@ class QuickSortQuestion
             sort(a, i + 1, r);
         }
     }
+}
 
+/**
+ * {@link https://en.wikipedia.org/wiki/Heapsort}。
+ */
+class HeapSortQuestion
+{
+
+    public static void sort(int[] a)
+    {
+        if (a == null || a.length <= 1)
+        {
+            return;
+        }
+        buildMaxHeap(a);
+        for (int i = a.length - 1; i >= 1; i--)
+        {
+            exchange(a, 0, i);
+            maxHeap(a, i, 0);
+        }
+    }
+
+    private static void buildMaxHeap(int[] a)
+    {
+        int half = a.length / 2;
+        for (int i = half; i >= 0; i--)
+        {
+            maxHeap(a, a.length, i);
+        }
+    }
+
+    private static void maxHeap(int[] a, int heapSize, int index)
+    {
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        int largest = index;
+        if (left < heapSize && a[left] > a[index])
+        {
+            largest = left;
+        }
+        if (right < heapSize && a[right] > a[largest])
+        {
+            largest = right;
+        }
+        if (index != largest)
+        {
+            exchange(a, index, largest);
+            maxHeap(a, heapSize, largest);
+        }
+    }
+
+    private static void exchange(int[] array, int index1, int index2)
+    {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
+
+/**
+ * {@link https://en.wikipedia.org/wiki/Merge_sort}。
+ */
+class MergeSortQuestion
+{
+    public static void sort(int[] a)
+    {
+        sort(a, 0, a.length - 1);
+    }
+
+    private static int[] sort(int[] a, int low, int high)
+    {
+        int mid = (low + high) / 2;
+        if (low < high)
+        {
+            // 左边
+            sort(a, low, mid);
+            // 右边
+            sort(a, mid + 1, high);
+            // 左右归并
+            merge(a, low, mid, high);
+        }
+        return a;
+    }
+
+    private static void merge(int[] nums, int low, int mid, int high)
+    {
+        int[] temp = new int[high - low + 1];
+        int i = low;// 左指针
+        int j = mid + 1;// 右指针
+        int k = 0;
+
+        // 把较小的数先移到新数组中
+        while (i <= mid && j <= high)
+        {
+            if (nums[i] <= nums[j])
+            {
+                temp[k++] = nums[i++];
+            } else
+            {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        // 把左边剩余的数移入数组
+        while (i <= mid)
+        {
+            temp[k++] = nums[i++];
+        }
+
+        // 把右边边剩余的数移入数组
+        while (j <= high)
+        {
+            temp[k++] = nums[j++];
+        }
+
+        // 把新数组中的数覆盖nums数组
+        for (int k2 = 0; k2 < temp.length; k2++)
+        {
+            nums[k2 + low] = temp[k2];
+        }
+    }
 }

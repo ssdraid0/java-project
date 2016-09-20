d07f6bb6d9c24c85aa976e6f655b3da2d0c345f9
ssdraid0 <ssdraid0@163.com>
2016-09-20 11:47:57 +0800

    新增IntroduceQuestion

---
+++ java/src/hr/IntroduceQuestion.java
@@ -0,0 +1,13 @@
+package hr;
+
+/**
+ * 1.自我介绍。<br>
+ * Java我已经学了两年了，现在我熟悉面向对象编程，集合框架，还有并发编程。 <br>
+ * MySQL数据库也学了两年，基本的增删查改操作，索引，数据库引擎我都比较熟悉。 <br>
+ * 另外我会用这个Spring框架，现在我能够搭建简单的整合Spring，MySQL和myBatis的web开发项目。 <br>
+ * 在开发过程中遇到的问题，我都是先查官方的文档，如果没找到，就去stackoverflow上搜索。<br>
+ */
+public class IntroduceQuestion
+{
+
+}
